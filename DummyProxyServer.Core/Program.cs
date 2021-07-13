668b80dc0c4568c64bbfd21b57d4433598b8c9a3
Mike Treit <mtreit@ntdev.microsoft.com>
2021-07-12 17:08:13 -0700

    .NET 5 version of the dummy proxy for comparison with the OWIN version.

---
+++ DummyProxyServer.Core/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace CampaignServiceWcfHost
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Logging;
+    using System;
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string hostName = "localhost";
+            int port = 1236;
+
+            if (args.Length > 1)
+            {
+                hostName = args[0];
+                if (!int.TryParse(args[1], out int temp))
+                {
+                    throw new ArgumentException("The port to use must be a valid integer.");
+                }
+
+                port = temp;
+            }
+
+            IWebHostBuilder builder = WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging
+                        .AddEventSourceLogger()
+                        .SetMinimumLevel(LogLevel.Warning);
+                })
+                .UseKestrel(options =>
+                {
+                    options.ListenAnyIP(port, (options) =>
+                    {
+                        options.UseHttps();
+                        Console.WriteLine($"Listening on port {port}.");
+                    });
+                })
+                .UseStartup<Startup>();
+
+            IWebHost host = builder.Build();
+
+            host.Run();
+            return 0;
+        }
+    }
+}
---
+++ DummyProxyServer.Core/Startup.cs
@@ -0,0 +1,75 @@
+﻿namespace CampaignServiceWcfHost
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient("redirectClient", config =>
+            {
+            }).ConfigurePrimaryHttpMessageHandler(() =>
+            {
+                HttpClientHandler handler = new()
+                {
+                    AutomaticDecompression = DecompressionMethods.All,
+                };
+
+                return handler;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHttpClientFactory clientFactory)
+        {
+            app.Run(async (context) =>
+            {
+                context.Response.Headers.Add("Content-Type", "text/xml; charset=utf-8");
+
+                await DoRequest(context, clientFactory);
+            });
+        }
+
+        static async Task DoRequest(HttpContext context, IHttpClientFactory clientFactory)
+        {
+            HttpClient client;
+            X509Certificate? clientCert = null;
+
+            var request = context.Request;
+
+            if (!request.Query.ContainsKey("SkipClientCert"))
+            {
+                clientCert = await context.Connection.GetClientCertificateAsync();
+            }
+
+            if (clientCert == null)
+            {
+                client = clientFactory.CreateClient("redirectClient");
+            }
+            else
+            {
+                var handler = new HttpClientHandler();
+                handler.ClientCertificates.Add(clientCert);
+                client = new HttpClient(handler);
+            }
+
+            var httpGetRequest = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://treit.github.io")
+            };
+
+            var response = await client.SendAsync(httpGetRequest, CancellationToken.None);
+            await context.Response.WriteAsync(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
